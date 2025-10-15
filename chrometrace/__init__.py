d09b0510c3b587dbe222fb19f95658acce14d6ab
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-10-15 16:09:43 +0200

    Add version attribute

--- chrometrace/__init__.py
+++ chrometrace/__init__.py
@@ -1,3 +1,6 @@
+import importlib.metadata
+
 from .chrometrace import ProcessTracer, TraceEvent, TraceEventType, TraceSink
 
+__version__ = importlib.metadata.version(__name__)
 __all__ = ["TraceEventType", "TraceEvent", "TraceSink", "ProcessTracer"]

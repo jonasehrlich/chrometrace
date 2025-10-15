401402876252e02d0fbc7b24413d9237020815c6
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-10-15 11:35:56 +0200

    Require Python 3.11
    This allows to remove our StrEnum implementation

--- chrometrace/chrometrace.py
+++ chrometrace/chrometrace.py
@@ -12,44 +12,8 @@ __all__ = ["TraceEventType", "TraceEvent", "TraceSink", "ProcessTracer"]
 
 JsonEncodable: ty.TypeAlias = ty.Any
 
-try:
-    from enum import StrEnum
-except ImportError:
-    # Backport of the StrEnum class from Python 3.11
-    class StrEnum(str, enum.Enum):
-        """
-        Enum where members are also (and must be) strings
-        """
 
-        def __new__(cls, *values):
-            "values must already be of type `str`"
-            if len(values) > 3:
-                raise TypeError("too many arguments for str(): %r" % (values,))
-            if len(values) == 1:
-                # it must be a string
-                if not isinstance(values[0], str):
-                    raise TypeError("%r is not a string" % (values[0],))
-            if len(values) >= 2:
-                # check that encoding argument is a string
-                if not isinstance(values[1], str):
-                    raise TypeError("encoding must be a string, not %r" % (values[1],))
-            if len(values) == 3:
-                # check that errors argument is a string
-                if not isinstance(values[2], str):
-                    raise TypeError("errors must be a string, not %r" % (values[2]))
-            value = str(*values)
-            member = str.__new__(cls, value)
-            member._value_ = value
-            return member
-
-        def _generate_next_value_(name, start, count, last_values):
-            """
-            Return the lower-cased version of the member name.
-            """
-            return name.lower()
-
-
-class TraceEventType(StrEnum):
+class TraceEventType(enum.StrEnum):
     """
     Type of a trace event
 
